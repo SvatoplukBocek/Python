5c34a3ed4f3e064b7aae6b38f0f1a0f9aa19274e
SvatoplukBocek <svatopluk.bocek@gmail.com>
2025-02-13 15:41:08 +0100

    Přidán print na 10 řádek

---
+++ Testovani.py
@@ -0,0 +1,9 @@
+cislo_1 = float(input("Zadejte první číslo:"+" "))
+cislo_2 = float(input("zadejte druhé číslo:"+" "))
+if cislo_1 == cislo_2:
+  print ("Obě čísla jsou stejná.")
+else:
+  if cislo_1>cislo_2:
+    print ("První číslo je větší než druhé")
+  else:
+    print ("První číslo je menší než druhé")
