c152d82a400e085eb67af6f36d55029af0e9be57
SvatoplukBocek <svatopluk.bocek@gmail.com>
2025-03-27 19:42:20 +0100

    Druhy pokus

--- main.py
+++ main.py
@@ -9,8 +9,29 @@ from random import randint
 
 hadane_cislo = str(randint(1000, 9999))         #cislo nemuze zacinat nulou, podmínka, aby nezacinalo nulou je zbytecna
 while len(set(hadane_cislo)) != 4:              #set zajistí unikátní prvky a len zajistí, aby byly 4
-    hadane_cislo = str(randint(1000,9999))
-    continue
+    hadane_cislo = str(randint(1000,9999))      #poznámka pro lektora: mě se zdá tvorba hádaného čísla na 3 řádky OK
+                                                #v čem je lepší to zabalit do funkce? Díky
+def kontrola_cisla(hadane_cislo, cislo):
+    """
+    Zkontroluje, kolik číslic a kolik pozic zadaného čísla se shoduje s hádaným číslem.
+    
+    Parametry:
+    hadane_cislo (str) – tajné číslo
+    cislo (str) – číslo, které uživatel zadal
+
+    Návrat:
+    (int, int) – počet správných číslic, počet správných pozic
+    """
+    stejna_pozice = 0
+
+    stejna_cislice = 0
+
+    for pozice in range(4):                                 #zkontroluje všechny číslice
+        if hadane_cislo[pozice] == cislo[pozice]:           #a porovná jestli jsou na stejných pozicích stejné číslice
+            stejna_pozice +=1
+        elif hadane_cislo[pozice] in cislo:                 #pokud nejsou, tak porovná, jestli číslice z pozice je v čísle samotném
+            stejna_cislice +=1
+    return stejna_pozice, stejna_cislice
 
 print("Hi there!\n"
 "-----------------------------------------------\n"
@@ -22,39 +43,14 @@ print("Hi there!\n"
 "-----------------------------------------------")
 cislo = 0
 
-pocet_pokusu = 0
-
-# print(hadane_cislo)                             #na zkoušení, pak smažu
+pocet_pokusu = 0                      
 
 while cislo != hadane_cislo:
     cislo = input()             #vse zadane pomoci input je string
-    pocet_pokusu += 1           #pocitani, za jak dlouho uhodne
+    pocet_pokusu +=1            #pocitani, za jak dlouho uhodne
     while cislo.startswith("0") or len(set(cislo)) != 4 or not cislo.isdigit():     #set zajistí unikátní prvky a len zajistí, aby byly 4
         print("Number can`t start with zero and must have 4 different digits, please input different number:")
         cislo = input() 
-        continue
-
-    def kontrola_cisla(hadane_cislo, cislo):
-        """
-        Zkontroluje, kolik číslic a kolik pozic zadaného čísla se shoduje s hádaným číslem.
-    
-        Parametry:
-        hadane_cislo (str) – tajné číslo
-        cislo (str) – číslo, které uživatel zadal
-
-        Návrat:
-        (int, int) – počet správných číslic, počet správných pozic
-        """
-        stejna_pozice = 0
-
-        stejna_cislice = 0
-
-        for pozice in range(4):                 #zkontroluje všechny číslice
-            if hadane_cislo[pozice] == cislo[pozice]:           #a porovná jestli jsou na stejných pozicích stejné číslice
-                stejna_pozice +=1
-            elif hadane_cislo[pozice] in cislo:                 #pokud nejsou, tak porovná, jestli číslice z pozice je v čísle samotném
-                stejna_cislice +=1
-        return stejna_pozice, stejna_cislice
 
     pozice, cislice = kontrola_cisla(hadane_cislo, cislo)
 
@@ -67,7 +63,6 @@ while cislo != hadane_cislo:
         print (f"Cow:{cislice}")
     else:
         print(f"Cows:{cislice}")
-    continue
 
 else:
     print("Correct, you've guessed the right number\n"          
