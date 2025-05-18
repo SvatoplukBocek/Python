1fa1ab31eb9011682a39d8c70264e5b185cd866b
SvatoplukBocek <svatopluk.bocek@gmail.com>
2025-05-18 09:04:44 +0200

    Projekt 3

---
+++ Projekt 3/main.py
@@ -0,0 +1,15 @@
+"""
+projekt_3.py: třetí projekt do Engeto Online Python Akademie
+
+author: Svatopluk Boček
+email: svatopluk.bocek@gmail.com
+"""
+
+def main():
+
+
+
+
+if __name__ == "__main__":
+    main()
+
